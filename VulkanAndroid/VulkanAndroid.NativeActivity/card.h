5c8b09224b8aa181fe25c260fb4097006cd919b1
Alexander Babey <Extinction132@gmail.com>
2019-03-16 14:36:36 +0300

    Modify card mesh

--- VulkanAndroid/VulkanAndroid.NativeActivity/card.h
+++ VulkanAndroid/VulkanAndroid.NativeActivity/card.h
@@ -6,15 +6,20 @@ namespace card
     const float A = 2.0f;
 
     const std::vector<PositionUv> VERTICES{
-        { glm::vec3(-1.0f, -2.0f, 0.0f), glm::vec2(0.0f, 0.0f) },
-        { glm::vec3(1.0f, -2.0f, 0.0f), glm::vec2(1.0f, 0.0f) },
-        { glm::vec3(1.0f, 0.0f, 0.0f), glm::vec2(1.0f, 1.0f) },
-        { glm::vec3(-1.0f, 0.0f, 0.0f), glm::vec2(0.0f, 1.0f) },
+        { glm::vec3(-1.0f, -2.15f, 0.0f), glm::vec2(-0.02f, -0.02f) },
+        { glm::vec3(1.0f, -2.15f, 0.0f), glm::vec2(1.02f, -0.02f) },
+        { glm::vec3(1.0f, -0.15f, 0.0f), glm::vec2(1.02f, 1.02f) },
+        { glm::vec3(-1.0f, -0.15f, 0.0f), glm::vec2(-0.02f, 1.02f) },
+
+        { glm::vec3(0.0f, 0.0f, 0.0f), glm::vec2(1.02, 1.02f) },
+        { glm::vec3(-0.05f, -0.15f, 0.0f), glm::vec2(1.02, 1.02f) },
+        { glm::vec3(0.05f, -0.15f, 0.0f), glm::vec2(1.02, 1.02f) },
     };
 
     const std::vector<uint32_t> INDICES{
         0, 1, 3,
         1, 2, 3,
+        4, 5, 6,
     };
 
 }
